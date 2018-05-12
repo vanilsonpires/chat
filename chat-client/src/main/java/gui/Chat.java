72a13f5140e901f06f64c3f62aa8da59ee82207b
Vanilson Pires <vanilsonpires@yahoo.com.br>
2018-05-12 18:15:42 -0300

    Criado projeto em branco

---
+++ chat-client/src/main/java/gui/Chat.java
@@ -0,0 +1,17 @@
+/**
+ * @author Vanilson Pires
+ * Date 12 de mai de 2018
+ */
+package gui;
+
+/**
+ * @author Vanilson Pires Date 12 de mai de 2018
+ *
+ */
+public class Chat {
+
+	public static void main(String[] args) {
+		new Gui();
+	}
+
+}
---
+++ chat-client/src/main/java/gui/Gui.java
@@ -0,0 +1,18 @@
+/**
+ * @author Vanilson Pires
+ * Date 12 de mai de 2018
+ */
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * Representa o frame principal
+ * 
+ * @author Vanilson Pires Date 12 de mai de 2018
+ *
+ */
+@SuppressWarnings("serial")
+public class Gui extends JFrame {
+
+}
